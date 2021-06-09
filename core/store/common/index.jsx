fdc656ae59b477b0fb5206a5cbd2f5a990b2b5a3
jskim <qwefk123@naver.com>
2021-06-09 22:01:48 +0900

    .

--- core/store/common/index.jsx
+++ core/store/common/index.jsx
@@ -1,13 +1,25 @@
+import React from 'react'
+
 import { AlertProvider, ConfirmProvider, LangProvider, UserProvider } from './providers'
+
+const Provider = ({ contexts, children }) =>
+	contexts.reduce(
+		(prev, context) =>
+			React.createElement(context, {
+				children: prev,
+			}),
+		children,
+	)
 
 export function CommonProvider({ children }) {
 	return (
-		<LangProvider>
-			<AlertProvider>
-				<ConfirmProvider>
-					<UserProvider>{children}</UserProvider>
-				</ConfirmProvider>
-			</AlertProvider>
-		</LangProvider>
+		// <LangProvider>
+		// 	<AlertProvider>
+		// 		<ConfirmProvider>
+		// 			<UserProvider>{children}</UserProvider>
+		// 		</ConfirmProvider>
+		// 	</AlertProvider>
+		// </LangProvider>
+		<Provider contexts={[AlertProvider, ConfirmProvider, LangProvider, UserProvider]}>{children}</Provider>
 	)
 }
